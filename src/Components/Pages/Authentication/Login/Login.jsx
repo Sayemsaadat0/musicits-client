68153debf5c3a78df87a5d824df70bd097194001
Sayemsaadat0 <sayemsaadat0@gmail.com>
2023-06-07 09:01:37 +0600

    backgrounds

--- src/Components/Pages/Authentication/Login/Login.jsx
+++ src/Components/Pages/Authentication/Login/Login.jsx
@@ -1,14 +1,15 @@
 import React, { useState } from 'react';
 import SocialLogin from '../SocialLogin/SocialLogin';
-import { BsEyeFill ,BsEyeSlashFill } from 'react-icons/bs'
+import { BsEyeFill, BsEyeSlashFill } from 'react-icons/bs'
 import { Link } from 'react-router-dom';
+import './Login.css'
 
 const Login = () => {
     const [passwordVisible, setPasswordVisible] = useState(false);
     const [password, setPassword] = useState('');
 
-    const handlePasswordToggle = () => {setPasswordVisible(!passwordVisible);};
-    const handlePasswordChange = (e) => {setPassword(e.target.value);};
+    const handlePasswordToggle = () => { setPasswordVisible(!passwordVisible); };
+    const handlePasswordChange = (e) => { setPassword(e.target.value); };
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -16,9 +17,9 @@ const Login = () => {
         console.log('Login submitted!');
     };
     return (
-        <div>
+        <div className=''>
 
-            <div className="flex h-screen w-full items-center justify-center bg-black  bg-cover bg-no-repeat" >
+            <div className="flex h-screen w-full items-center justify-center background  bg-cover bg-no-repeat" >
                 <div className="rounded-xl bg-gray-900 shadow-lg hover:shadow-red-500 px-16 py-10  backdrop-blur-md max-sm:px-8">
                     <div className="text-white">
                         <div className="mb-8 flex flex-col items-center">
@@ -28,21 +29,23 @@ const Login = () => {
                         <form onSubmit={handleSubmit}>
                             <div className="mb-4 text-lg">
                                 <input
-                                    className="border-none bg-red-500 px-6 py-2 placeholder-white text-center  shadow-lg"
+                                    className="border-none bg-red-500 px-6 py-2 placeholder-white text-center rounded-3xl w-full shadow-lg"
                                     type="text"
                                     name="name"
-                                    placeholder="Enter Your Email"/>
+                                    placeholder="Enter Your Email" />
                             </div>
                             <div>
-                                <div className="input-group-sm">
+                                <div className="input-group-sm ">
                                     <input
-                                         type={passwordVisible ? 'text' : 'password'}
-                                         id="password"
-                                         value={password}
-                                         onChange={handlePasswordChange} 
-                                        placeholder="password"
-                                        className="border-none bg-red-500 px-6 py-2 placeholder-white text-center  shadow-lg"/>
-                                    <span onClick={handlePasswordToggle} className="btn btn-ghost ">
+                                        type={passwordVisible ? 'text' : 'password'}
+                                        id="password"
+                                        value={password}
+                                        onChange={handlePasswordChange}
+                                        className="rounded-3xl w-full border-none bg-red-500 px-6 py-2 placeholder-white text-center shadow-lg"
+                                        name="name"
+                                        placeholder="Your Password" />
+                                    <span
+                                        onClick={handlePasswordToggle} className="btn btn-ghost absolute right-16 rounded-3xl">
                                         {passwordVisible ? <BsEyeFill></BsEyeFill> : <BsEyeSlashFill></BsEyeSlashFill>}
                                     </span>
                                 </div>
@@ -50,10 +53,10 @@ const Login = () => {
                             <div className="mt-8 flex justify-center text-lg text-black">
                                 <button
                                     type="submit"
-                                    className="bg-red-500 px-10 py-2 text-white w-full hover:bg-red-700">Login</button>
+                                    className="bg-red-500 px-10 py-2 text-white w-full hover:bg-red-700 rounded-3xl">Login</button>
                             </div>
                             <div className='mt-4'>
-                                    <p>Do not have  an account? <Link className='text-red-500' to='/signup'>Sign Up</Link> here</p> 
+                                <p>Do not have  an account? <Link className='text-red-500' to='/signup'>Sign Up</Link> here</p>
                             </div>
                             <div className='mx-auto text-center mt-10'>
                                 <p>or login with</p>
--- src/Components/Pages/Authentication/SignUp/SignUp.jsx
+++ src/Components/Pages/Authentication/SignUp/SignUp.jsx
@@ -1,40 +1,65 @@
+import { Link } from "react-router-dom";
 import SocialLogin from "../SocialLogin/SocialLogin";
 
 const SignUp = () => {
     return (
         <div>
 
-            <div className="flex h-screen w-full items-center justify-center bg-black  bg-cover bg-no-repeat" >
+            <div className="flex h-screen w-full items-center justify-center background  bg-cover bg-no-repeat" >
                 <div className="rounded-xl bg-gray-900 shadow-lg hover:shadow-red-500 px-16 py-10  backdrop-blur-md max-sm:px-8">
                     <div className="text-white">
                         <div className="mb-8 flex flex-col items-center">
-                            <h1 className="mb-2 text-2xl">Login Here</h1>
+                            <h1 className="mb-2 text-2xl">Sign Up here</h1>
                             <span className="">Enter Your Details</span>
                         </div>
                         <form className=' '>
                             <div className="mb-4 text-lg">
                                 <input
-                                    className="rounded-3xl border-none bg-red-500 px-6 py-2 placeholder-white text-center shadow-lg"
+                                    className="rounded-3xl w-full border-none bg-red-500 px-6 py-2 placeholder-white text-center shadow-lg"
                                     type="text"
                                     name="name"
+                                    placeholder="Your Name" />
+                            </div>
+                            <div className="mb-4 text-lg">
+                                <input
+                                    className="rounded-3xl w-full border-none bg-red-500 px-6 py-2 placeholder-white text-center shadow-lg"
+                                    type="email"
+                                    name="email"
                                     placeholder="Enter Your Email" />
+                            </div>
+                            <div className="mb-4 text-lg">
+                                <input
+                                    className="rounded-3xl w-full border-none bg-red-500 px-6 py-2 placeholder-white text-center shadow-lg"
+                                    type="text"
+                                    name="photo"
+                                    placeholder="Photo URl" />
                             </div>
 
                             <div className="mb-4 text-lg">
                                 <input
-                                    className="rounded-3xl border-none bg-red-500 px-6 py-2 placeholder-white text-center shadow-lg"
+                                    className="rounded-3xl w-full border-none bg-red-500 px-6 py-2 placeholder-white text-center shadow-lg"
                                     type="Password"
-                                    name="name"
+                                    name="password"
                                     placeholder="Your Password" />
+                            </div>
+                            <div className="mb-4 text-lg">
+                                <input
+                                    className="rounded-3xl w-full border-none bg-red-500 px-6 py-2 placeholder-white text-center shadow-lg"
+                                    type="Password"
+                                    name="confirm"
+                                    placeholder="Confirm Your Password" />
                             </div>
                             <div className="mt-8 flex justify-center text-lg text-black">
                                 <button
                                     type="submit"
                                     className="rounded-3xl w-full bg-red-500  px-10 py-2 text-white shadow-xl backdrop-blur-md transition-colors duration-300 hover:bg-red-600">Sign up</button>
                             </div>
+                            <div className='mt-4'>
+                                <p>Already have  an account? <Link className='text-red-500' to='/login'>Login</Link> here</p>
+                            </div>
                             <div className='mx-auto text-center mt-10'>
                                 <p>or login with</p>
-                               <SocialLogin></SocialLogin>
+                                <SocialLogin></SocialLogin>
                             </div>
                         </form>
                     </div>

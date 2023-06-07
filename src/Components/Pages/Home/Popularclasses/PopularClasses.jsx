60835e8ca24203ccb92dada694aefba68051233a
Sayemsaadat0 <sayemsaadat0@gmail.com>
2023-06-07 15:09:20 +0600

    popolar class

--- src/Components/Pages/Home/Popularclasses/PopularClasses.jsx
+++ src/Components/Pages/Home/Popularclasses/PopularClasses.jsx
@@ -36,15 +36,23 @@ const PopularClasses = () => {
         modules={[Pagination]}
         className="mySwiper p-5 mt-10"
       >
-        <SwiperSlide><img src='https://i.pinimg.com/564x/d2/8f/44/d28f440e2bdb78c3c50fb24eb4f02897.jpg' alt="" /></SwiperSlide>
-        <SwiperSlide><img src='https://i.pinimg.com/564x/9e/85/b8/9e85b8673139d952521973586dfdf4ab.jpg' alt="" /></SwiperSlide>
-        <SwiperSlide><img src='https://i.pinimg.com/564x/d7/82/6f/d7826f30c292c1f0ec5dc0c43cf3f82c.jpg' alt="" /></SwiperSlide>
-        <SwiperSlide><img src='https://i.pinimg.com/564x/8b/24/03/8b24038ff83ff27aa7dd2e2e5281b3d5.jpg' alt="" /></SwiperSlide>
-        <SwiperSlide><img src='https://i.pinimg.com/564x/87/af/5d/87af5db1259275314a4aaa933e7c1517.jpg' alt="" /></SwiperSlide>
-        <SwiperSlide><img src='https://i.pinimg.com/564x/7b/60/40/7b6040d53506ca2f330f4ba9b562a7b2.jpg' alt="" /></SwiperSlide>
-        <SwiperSlide><img src='https://i.pinimg.com/564x/45/b2/67/45b2674e1aeb70b866e246d5e4d68334.jpg' alt="" /></SwiperSlide>
-        <SwiperSlide><img src='https://i.pinimg.com/564x/dd/30/88/dd3088c7e2f077e2a68687af1f7491d1.jpg' alt="" /></SwiperSlide>
-        <SwiperSlide><img src='https://i.pinimg.com/564x/7f/c4/e6/7fc4e69606e21bf36712cc7b955dbd59.jpg' alt="" /></SwiperSlide>
+        <SwiperSlide><img src='https://i.ibb.co/YkbzQ7H/1.png' alt="" /></SwiperSlide>
+
+        <SwiperSlide><img src='https://i.ibb.co/ssKcRMK/2.png' alt="" /></SwiperSlide>
+
+        <SwiperSlide><img src='https://i.ibb.co/sjqk2pv/3.png' alt="" /></SwiperSlide>
+
+        <SwiperSlide><img src='https://i.ibb.co/74j0Bp1/4.png' alt="" /></SwiperSlide>
+
+        <SwiperSlide><img src='https://i.ibb.co/YNdQGSv/5.png' alt="" /></SwiperSlide>
+
+        <SwiperSlide><img src='https://i.ibb.co/L9j9w0f/6.png' alt="" /></SwiperSlide>
+
+        <SwiperSlide><img src='https://i.ibb.co/TMmPHNG/7.png' alt="" /></SwiperSlide>
+
+        <SwiperSlide><img src='https://i.ibb.co/Nx4L0zH/8.png' alt="" /></SwiperSlide>
+
+        <SwiperSlide><img src='https://i.ibb.co/nPV8bP8/9.png' alt="" /></SwiperSlide>
        
       
       </Swiper>
@@ -54,3 +62,12 @@ const PopularClasses = () => {
 };
 
 export default PopularClasses;
+
+
+
+
+
+
+
+
+
